f8947d164a3fd1e7b889af09d98847e9e79a60bb
maxsa <maxsa@DESKTOP-169QVNN>
2016-07-24 02:24:07 +0300

    Добавьте файлы проекта.

---
+++ Cgf+itg/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Cgf_itg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.itg = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.GO = new System.Windows.Forms.Button();
+            this.Linkitg = new System.Windows.Forms.TextBox();
+            this.Linkcfg = new System.Windows.Forms.TextBox();
+            this.Linknew = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.NameNewFile = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // itg
+            // 
+            this.itg.Enabled = false;
+            this.itg.Location = new System.Drawing.Point(345, 16);
+            this.itg.Name = "itg";
+            this.itg.Size = new System.Drawing.Size(75, 23);
+            this.itg.TabIndex = 0;
+            this.itg.Text = "*.itg";
+            this.itg.UseVisualStyleBackColor = true;
+            this.itg.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(345, 45);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "*.cfg";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(426, 74);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "new *.itg";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // GO
+            // 
+            this.GO.Location = new System.Drawing.Point(524, 16);
+            this.GO.Name = "GO";
+            this.GO.Size = new System.Drawing.Size(75, 51);
+            this.GO.TabIndex = 3;
+            this.GO.Text = "GO";
+            this.GO.UseVisualStyleBackColor = true;
+            this.GO.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Linkitg
+            // 
+            this.Linkitg.Enabled = false;
+            this.Linkitg.Location = new System.Drawing.Point(6, 18);
+            this.Linkitg.Name = "Linkitg";
+            this.Linkitg.Size = new System.Drawing.Size(333, 20);
+            this.Linkitg.TabIndex = 4;
+            this.Linkitg.Text = "путь к архиву ";
+            // 
+            // Linkcfg
+            // 
+            this.Linkcfg.Enabled = false;
+            this.Linkcfg.Location = new System.Drawing.Point(6, 47);
+            this.Linkcfg.Name = "Linkcfg";
+            this.Linkcfg.Size = new System.Drawing.Size(333, 20);
+            this.Linkcfg.TabIndex = 5;
+            this.Linkcfg.Text = "путь к архиву ";
+            // 
+            // Linknew
+            // 
+            this.Linknew.Enabled = false;
+            this.Linknew.Location = new System.Drawing.Point(6, 76);
+            this.Linknew.Name = "Linknew";
+            this.Linknew.Size = new System.Drawing.Size(333, 20);
+            this.Linknew.TabIndex = 6;
+            this.Linknew.Text = "для хранения временных файлов и нового файла";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Linkitg);
+            this.groupBox1.Controls.Add(this.NameNewFile);
+            this.groupBox1.Controls.Add(this.GO);
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.Linknew);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.itg);
+            this.groupBox1.Controls.Add(this.Linkcfg);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(605, 102);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Настройки";
+            // 
+            // NameNewFile
+            // 
+            this.NameNewFile.Location = new System.Drawing.Point(345, 76);
+            this.NameNewFile.Name = "NameNewFile";
+            this.NameNewFile.Size = new System.Drawing.Size(75, 20);
+            this.NameNewFile.TabIndex = 8;
+            this.NameNewFile.Text = "1.itg";
+            this.NameNewFile.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(675, 177);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Go";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button itg;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button GO;
+        private System.Windows.Forms.TextBox Linkitg;
+        private System.Windows.Forms.TextBox Linkcfg;
+        private System.Windows.Forms.TextBox Linknew;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox NameNewFile;
+    }
+}
+
---
+++ Cgf+itg/Form1.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ionic.Zip;
+using System.IO;
+
+
+namespace Cgf_itg
+{
+    public partial class Form1 : Form
+    {
+        public OpenFileDialog openFiles;
+        public string archive;
+        public ZipFile zip;
+        public SaveFileDialog saveFile;
+        public FolderBrowserDialog saveDialog;
+        public string path = "";
+        public string newFolderName = "";
+        public string newFolderName1 = "";
+        public string newFolderName2 = "";
+        public string a1 = "";
+        public string a2 = "";
+        public string a3 = "";
+        public string a4 = "";
+
+        public int j =0;
+        public Form1()
+       
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFiles = new OpenFileDialog();
+                openFiles.Title = "Выберите архив, который необходимо разархивировать";
+
+                if (openFiles.ShowDialog() == DialogResult.OK)
+                {
+                    archive = openFiles.FileName;
+                    Linkitg.Text = archive;
+                }
+                else return;
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка во время выбора архива для разархивации, попробуйте еще раз! " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            
+            try
+            {
+
+                //Создаем объект для работы с архивом
+                using (zip = new ZipFile(archive, Encoding.UTF8))
+                {
+                    //Задаем системную директорию TEMP для временных файлов
+                    zip.TempFileFolder = System.IO.Path.GetTempPath();
+                    //Добавляем файл и указываем где он будет располагаться в архиве
+                    zip.ExtractAll(newFolderName1, ExtractExistingFileAction.OverwriteSilently);
+                    zip = null;
+                }
+
+                MessageBox.Show("Данные успешно извлечены", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка при попытки сохранения архива, попробуйте еще раз! " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+            //копируем в tmp файл для работы
+            File.Copy(Path.Combine(newFolderName1, "obd1_slp.itg"), Path.Combine(newFolderName, "obd1_slp.itg"), true);
+            
+            //удаляем временную папку для архива
+            DirectoryInfo source1 = new DirectoryInfo(newFolderName1);
+            source1.Delete(true);
+            //закрыть выбор для извлчения файлов из архива
+            itg.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                openFiles = new OpenFileDialog();
+                openFiles.Title = "Выберите архив, который необходимо разархивировать";
+
+                if (openFiles.ShowDialog() == DialogResult.OK)
+                {
+                    archive = openFiles.FileName;
+                    Linkcfg.Text = archive;
+                }
+                else return;
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка во время выбора архива для разархивации, попробуйте еще раз! " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            
+            try
+            {
+
+                //Создаем объект для работы с архивом
+                using (zip = new ZipFile(archive, Encoding.UTF8))
+                {
+                    //Задаем системную директорию TEMP для временных файлов
+                    zip.TempFileFolder = System.IO.Path.GetTempPath();
+                    //Добавляем файл и указываем где он будет располагаться в архиве
+                    zip.ExtractAll(newFolderName2, ExtractExistingFileAction.OverwriteSilently);
+                    zip = null;
+                }
+
+                MessageBox.Show("Данные успешно извлечены", "Информация", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+            }
+            catch (Exception ex) { MessageBox.Show("Ошибка при попытки сохранения архива, попробуйте еще раз! " + ex.Message, "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            
+            //копируем в tmp файл для работы
+            File.Copy(Path.Combine(newFolderName2, "Diagn.cfg"), Path.Combine(newFolderName, "Diagn.cfg"), true);
+
+            //удаляем временную папку для архива
+            DirectoryInfo source2 = new DirectoryInfo(newFolderName2);
+            source2.Delete(true);
+            //закрыть выбор для извлчения файлов из архива
+            button2.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            // тут обработка и работа с файлами
+
+            //File.Create(newFolderName + "\\"+ NameNewFile.Text);
+            var encod = System.Text.Encoding.GetEncoding(866);
+            System.IO.StreamWriter textFile = new System.IO.StreamWriter(newFolderName + "\\" + NameNewFile.Text);
+            textFile.Close();
+
+            int count = System.IO.File.ReadAllLines(newFolderName +  "\\Diagn.cfg").Length;
+            int count1 = System.IO.File.ReadAllLines(newFolderName + "\\obd1_slp.itg").Length;
+
+            StreamReader fileIn = new StreamReader(newFolderName + "\\obd1_slp.itg", encod);
+            string line = null;
+
+            StreamWriter fileOut = new StreamWriter(newFolderName + "\\" + NameNewFile.Text, true, encod);
+
+            while ((line = fileIn.ReadLine()) != "/*BDSLP")
+            {
+             fileOut.WriteLine(line);
+            }
+            fileOut.WriteLine("/*BDSLP");
+            
+            //while ((line = fileIn.ReadLine()) == "/*BDSHGS")
+            //{
+            //
+            //    fileOut.WriteLine(line);
+            //
+            //    for (int i = 0; i >= count; i++)
+            //    {
+            //        line = fileIn.ReadLine();
+            //        fileOut.WriteLine(line);
+            //    }
+            //}
+            fileIn.Close();
+            fileOut.Close();
+            //поиск из файла Diagn.cfg  Input Period
+            StreamReader fileInCfg = new StreamReader(newFolderName + "\\Diagn.cfg", encod);
+            string line1 = null;
+            
+            StreamWriter fileOutNew = new StreamWriter(newFolderName + "\\" + NameNewFile.Text, true, encod);
+
+                for (int i = 0; i <= count; i++)
+                {
+                if ((line1 = fileInCfg.ReadLine()) == "	Block	[00001]	Num 5 LN 4 Version 0x0 Type Input Period 100000	{ ")
+                {
+                    while ((line1 = fileInCfg.ReadLine()) != "	 }  Size Block 399 Count Buff 30 Size 399;")
+                    {
+                        string[] split = line1.Split(new Char[] { '"', '.', '.', '"',' ', '"', ';','\t' }, StringSplitOptions.RemoveEmptyEntries).Skip(1).ToArray();
+
+                        foreach (string s in split)
+                        {
+
+                            if (s.Trim() != "")
+                            if (s!="Off")
+                            {
+                                    //числа не записывать в новый файл!!!
+                                    bool IsDigit = s.All(char.IsDigit);
+
+                                    if (!IsDigit)
+                                    {
+                                   
+                                            j++;
+                                            
+                                                if (j==1)
+                                                {
+                                                    a1 = s;
+                                                }
+                                                if (j == 2)
+                                                {
+                                                    a2 = s;
+                                                }
+                                                if (j == 3)
+                                                {
+                                                    a3 = s;
+                                                }
+                                        if (j == 4)
+                                        {
+                                            if (s == "UINT" && (a3 == "COUNT_ST_BKZ" | a3 == "M_NOMER" | a3 == "VP"))
+                                            {
+                                                a4 = "INT";
+
+                                                fileOutNew.WriteLine("#ТЕХ ПРЕФИКС=" + a1 + ", ПАРАМ=" + a2 + ", АТР=" + a3 + ", ТИП=" + a4);
+                                                j = 0;
+                                            }
+                                            else
+                                            {
+                                                if (s != "UINT")
+                                                {
+                                                    a4 = s;
+                                                    fileOutNew.WriteLine("#ТЕХ ПРЕФИКС=" + a1 + ", ПАРАМ=" + a2 + ", АТР=" + a3 + ", ТИП=" + a4);
+                                                    j = 0;
+                                                }
+                                                else
+                                                {
+                                                    a4 = "BOOL";
+
+                                                    fileOutNew.WriteLine("#ТЕХ ПРЕФИКС=" + a1 + ", ПАРАМ=" + a2 + ", АТР=" + a3 + ", ТИП=" + a4);
+                                                    j = 0;
+                                                }
+                                            }
+                                        }
+
+                                            
+
+                                        
+                                    }
+                                    
+
+                                }
+                                
+                        }
+                    }
+                }
+
+            }
+            fileInCfg.Close();
+            //fileOutNew.Close();
+            StreamReader fileInItg = new StreamReader(newFolderName + "\\obd1_slp.itg", encod);
+            string line2 = null;
+            for (int i = 0; i <= count1; i++)
+            {
+                if ((line1 = fileInItg.ReadLine()) == "/*BDSHGS")
+                {
+                    while ((line2 = fileInItg.ReadLine()) != " #КОНЕЦ")
+                    {
+                        fileOutNew.WriteLine(line2);
+                    }
+
+                }
+                    }
+            fileOutNew.WriteLine("#КОНЕЦ");
+            fileInItg.Close();
+            fileOutNew.Close();
+            //удалить файлы лишние 
+
+
+            // открыть место файла
+            System.Diagnostics.Process.Start("explorer.exe", newFolderName );
+
+            //закрыть обработчик
+            Close();
+
+
+            //         while ((line1 = fileInCfg.ReadLine()) != "Input Period")
+            //         {
+            //
+            //         
+            //         while ((line1 = fileInCfg.ReadLine()) == "Input Period")
+            //         {
+            //             while ((line1 = fileInCfg.ReadLine()) != "Size Block")
+            //             {
+            //                 string[] split = line1.Split(new Char[] {'"','.', '.', '"', '"','"'});
+            //
+            //                 foreach (string s in split)
+            //                 {
+            //
+            //                     if (s.Trim() != "")
+            //                         fileOutNew.WriteLine(s);
+            //                 }
+            //             }
+            //         }
+            //         }
+            //
+
+
+
+
+            //StreamReader fileInCfg = new StreamReader(newFolderName + "\\Diagn.cfg", encod);
+            //string line1 = null;
+            //
+            //StreamWriter fileOutNew = new StreamWriter(newFolderName + "\\" + NameNewFile.Text, true, encod);
+            //
+            //while ((line = fileIn.ReadLine()) == "Input Period")
+            //{
+            //    while ((line = fileIn.ReadLine()) != "Size Block")
+            //                    
+            //    for (int i = 0; i <= line.Length; i++)
+            //    {
+            //            char p = line[i];
+            //           
+            //    }
+            //    //fileOut.WriteLine(line);
+            //}
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            
+            saveDialog = new FolderBrowserDialog();
+            saveDialog.Description = "Выберите папку для разархивации";
+
+            if (saveDialog.ShowDialog() == DialogResult.OK)
+            {
+                path = saveDialog.SelectedPath;
+
+                DirectoryInfo drInfo1 = new DirectoryInfo(path + "\\tmp1");
+                drInfo1.Create();
+                newFolderName1 = drInfo1.FullName;
+                DirectoryInfo drInfo2 = new DirectoryInfo(path + "\\tmp2");
+                drInfo2.Create();
+                newFolderName2 = drInfo2.FullName;
+                DirectoryInfo drInfo = new DirectoryInfo(path+"\\tmp");
+                drInfo.Create();
+
+                Linknew.Text = drInfo.FullName;
+                newFolderName = drInfo.FullName;
+            }
+            button3.Enabled = false;
+            button2.Enabled = true;
+            itg.Enabled = true;
+            NameNewFile.Enabled = false;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (NameNewFile.Text == "Имя файла")
+            {
+                button3.Enabled = false;
+            }
+            else
+                button3.Enabled = true;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GO_EnabledChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void GO_EnabledChanged_1(object sender, EventArgs e)
+        {
+        
+        }
+
+
+        private void GO_VisibleChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
